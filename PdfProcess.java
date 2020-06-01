40d27bda20b3b9b7d20287df6f57e9193794c24c
Tokichan <48743258+Tokichan@users.noreply.github.com>
2020-06-01 18:03:43 +0800

    see comment to use

--- PdfProcess.java
+++ PdfProcess.java
@@ -1,17 +1,13 @@
 package com.example.myapplication;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapRegionDecoder;
-import android.graphics.ImageDecoder;
+import android.os.Environment;
+import android.util.Log;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.RectangleReadOnly;
-import com.itextpdf.text.pdf.ByteBuffer;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.ByteArrayOutputStream;
@@ -20,26 +16,44 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class PdfProcess {
-    public static void creatPdfOfImages(ArrayList<Bitmap> bitmaps,String filePath,String fileName) throws IOException, DocumentException {
+    //How to use this method:
+    /*
+    ArrayList<Bitmap> m = new ArrayList<Bitmap>();
+    m.add(bitmap1);
+    m.add(bitmap);
+    try {
+        PdfProcess.createPdfOfImages(m,"test.pdf");
+    } catch (IOException e) {
+        e.printStackTrace();
+    } catch (DocumentException e) {
+        e.printStackTrace();
+    }
+    */
+    
+    public static void createPdfOfImages(ArrayList<Bitmap> bitmaps, String fileName) throws IOException, DocumentException {
         Document doc = new Document();
-        PdfWriter.getInstance(doc,new FileOutputStream(filePath+"/"+fileName+".pdf"));
-        doc.open();
-
         float maxHeight = 0;
         float maxWidth = 0;
         for(Bitmap b:bitmaps){
             maxHeight = maxHeight>b.getHeight()?maxHeight:b.getHeight();
             maxWidth = maxWidth>b.getWidth()?maxWidth:b.getWidth();
         }
-        doc.setPageSize(new RectangleReadOnly(maxWidth,maxHeight));
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        //doc.setPageSize(PageSize.A4);
+        doc.setPageSize(new RectangleReadOnly((float)(maxWidth*1),(float)(maxHeight*1)));
+        doc.setMargins(0,0,0,0);
+        PdfWriter.getInstance(doc,new FileOutputStream(Environment.getExternalStorageDirectory()+"/"+fileName));
+        //Log.d("PDF",Environment.getExternalStorageDirectory()+"/"+fileName);
+        doc.open();
 
         for(Bitmap b:bitmaps){
-            b.compress(Bitmap.CompressFormat.JPEG,100,output);
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            //Log.d("IMG",String.format("%d %d%n",b.getWidth(),b.getHeight()));
+            b.compress(Bitmap.CompressFormat.PNG,100,output);
             Image img = Image.getInstance(output.toByteArray());
+            img.setAbsolutePosition((maxWidth-b.getWidth())/2,(maxHeight-b.getHeight())/2);
             doc.add(img);
+            doc.newPage();
         }
-
         doc.close();
     }
 
