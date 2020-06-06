5d214fb1ba5d508a21840be411da54cb290e3935
Tokichan <48743258+Tokichan@users.noreply.github.com>
2020-06-06 14:38:58 +0800

    adding waiting animation

--- PdfProcess.java
+++ PdfProcess.java
@@ -1,10 +1,18 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -33,11 +41,11 @@ public class PdfProcess {
     ArrayList<Bitmap> m = new ArrayList<Bitmap>();
     m.add(bitmap1);
     m.add(bitmap);
-    PdfProcess.createPdfOfImages(this,m,"test.pdf");
+    PdfProcess.createPdfOfImages(MainActivity.this,m,"test.pdf");
 
     */
 
-    public static void createPdfOfImages(Activity activity, final ArrayList<Bitmap> bitmaps, String fileName){
+    public static void createPdfOfImages(final Activity activity, final ArrayList<Bitmap> bitmaps, String fileName){
         final ProgressDialog dialog = ProgressDialog.show(activity, "正在產生PDF...", "請稍後", true);
         fileName = fileName.equals("") ? getDateTime() + ".pdf" : fileName;
         fileName = fileName.endsWith(".pdf") ? fileName : fileName + ".pdf";
@@ -46,7 +54,6 @@ public class PdfProcess {
             @Override
             public void run() {
                 try {
-
                     Document doc = new Document();
                     float maxHeight = 0;
                     float maxWidth = 0;
@@ -54,33 +61,50 @@ public class PdfProcess {
                         maxHeight = maxHeight > b.getHeight() ? maxHeight : b.getHeight();
                         maxWidth = maxWidth > b.getWidth() ? maxWidth : b.getWidth();
                     }
-                    //doc.setPageSize(PageSize.A4);
                     doc.setPageSize(new RectangleReadOnly((float) (maxWidth * 1), (float) (maxHeight * 1)));
                     doc.setMargins(0, 0, 0, 0);
                     PdfWriter.getInstance(doc, new FileOutputStream(Environment.getExternalStorageDirectory() + "/" + finalFileName));
-                    //Log.d("PDF",Environment.getExternalStorageDirectory()+"/"+fileName);
                     doc.open();
-
                     for (Bitmap b : bitmaps) {
                         ByteArrayOutputStream output = new ByteArrayOutputStream();
-                        //Log.d("IMG",String.format("%d %d%n",b.getWidth(),b.getHeight()));
                         b.compress(Bitmap.CompressFormat.PNG, 100, output);
                         Image img = Image.getInstance(output.toByteArray());
                         img.setAbsolutePosition((maxWidth - b.getWidth()) / 2, (maxHeight - b.getHeight()) / 2);
                         doc.add(img);
                         doc.newPage();
+                        output.close();
                     }
                     doc.close();
                 } catch (Exception e) {
                     Log.d("EXC", e.toString());
                 }finally {
                     dialog.dismiss();
+                    Looper.prepare();
+                    @SuppressLint("HandlerLeak") Handler handler = new Handler(){
+                        @Override
+                        public void handleMessage(@NonNull Message msg) {
+                            super.handleMessage(msg);
+                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                            builder.setMessage("成功建立PDF!");
+                            builder.setNegativeButton("確定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                            AlertDialog alertDialog = builder.create();
+                            alertDialog.show();
+                        }
+
+                    };
+                    handler.sendEmptyMessage(0);
+                    Looper.loop();
+
                 }
             }
         }).start();
     }
 
-    private static String getDateTime(){
+    public static String getDateTime(){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault());
         return sdf.format(new Date());
     }
