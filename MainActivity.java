2eeefb6faf1e5945fba2d31d5059c295d1038a05
Tokichan <48743258+Tokichan@users.noreply.github.com>
2020-06-08 18:29:09 +0800

    Here's an empty MainActivity example
    with select image and create PDF part done ,
    please put your code in "CODE HERE" and call selectImageAndCreatePdf() to activate the function.

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,78 @@
-package ntou.cs.java.projects;
+package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View.OnTouchListener;
+import android.widget.RelativeLayout;
+
+import org.opencv.core.Point;
+import org.opencv.osgi.OpenCVNativeLoader;
+import org.opencv.ximgproc.ContourFitting;
+
+import android.util.Log;
 import android.widget.TextView;
 
+import com.example.myapplication.ImageProcess;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Uri> imageData;//add this to the top as var
+
+    public ArrayList<Bitmap> bitmaps = new ArrayList<>();
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*
+        * CODE HERE
+        * CODE HERE
+        * CODE HERE
+        * CODE HERE
+        * */
+
+        //call below to activate image selection and create pdf
+        //selectImageAndCreatePdf();
+        
     }
-    public void btnAction(View view){
-        setImageData();
-    }
-    public void setTextView(){//just an example of manipulating list of Uris
-        TextView textView = (TextView)findViewById(R.id.textView);
-        textView.setText("");
-        for(Uri data:getImageLocation()){
-            textView.append(data.toString()+"\n");
-        }
-    }
-    public void setImageData(){
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public void selectImageAndCreatePdf(){
         Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        startActivityForResult(intent,1234);
+        startActivityForResult(intent,1);
     }
     @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode==1234){
+        if(requestCode==1){
             if(resultCode==RESULT_OK){
                 imageData=new ArrayList<>();
                 if(data.getClipData() != null){
@@ -49,7 +86,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        setTextView();//This is where your code goes if you need to use the data,delete the setTextView and change it to your function
+
+        if(imageData!=null){
+            for(Uri temp:imageData){
+                try {
+                    bitmaps.add(MediaStore.Images.Media.getBitmap(this.getContentResolver(), temp));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            PdfProcess.createPdfOfImagesWithUserInput(this,bitmaps);
+        }
+
     }
     public ArrayList<Uri> getImageLocation(){
         return imageData;//return a ArrayList of Uri, so please consider there may be multiple data
